2a4423f6a83452383a2147bbe7ed70c1babbce15
Ramiro Alvarez <ramiro.alvarez@aguayo.co>
2022-08-09 15:45:54 -0500

    Fix(Storages) - Soporte para S3, Minio o Sistema de archivos local

--- vsm_snw/settings/base.py
+++ vsm_snw/settings/base.py
@@ -273,25 +273,25 @@ if DEBUG:
 # Storage
 # https://django-storages.readthedocs.io/en/latest/index.html
 # https://django-storages.readthedocs.io/en/latest/backends/amazon-S3.html
-DEFAULT_FILE_STORAGE = os.environ.get("DEFAULT_FILE_STORAGE")
+DEFAULT_FILE_STORAGE = os.environ.get("DEFAULT_FILE_STORAGE", "storages.backends.s3boto3.S3Boto3Storage")
 if DEFAULT_FILE_STORAGE == "storages.backends.s3boto3.S3Boto3Storage":
-    AWS_DEFAULT_ACL = None
-    AWS_S3_FILE_OVERWRITE = False
+    AWS_DEFAULT_ACL = "public-read"
+    AWS_S3_FILE_OVERWRITE = True
+    AWS_S3_ENDPOINT_URL = os.environ.get("AWS_S3_ENDPOINT_URL", None)
     AWS_QUERYSTRING_EXPIRE = 60 * 60 * 24 * 7  # 1 week
-    AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
-    AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
-    if os.environ.get("AWS_STORAGE_BUCKET_NAME"):
+    AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID", None)
+    AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY", None)
+    if os.environ.get("AWS_STORAGE_BUCKET_NAME", None) is not None:
         AWS_STORAGE_BUCKET_NAME = os.environ.get("AWS_STORAGE_BUCKET_NAME")
     AWS_S3_REGION_NAME = os.environ.get("AWS_S3_REGION_NAME", None)
-    AWS_S3_CUSTOM_DOMAIN = os.environ.get("AWS_S3_CUSTOM_DOMAIN")
     AWS_S3_OBJECT_PARAMETERS = {
         "CacheControl": f"max-age={60 * 60 * 24 * 7 * 4}"  # 4 weeks
     }
-    if os.environ.get("AWS_QUERYSTRING_AUTH"):
+    if os.environ.get("AWS_QUERYSTRING_AUTH", None) is not None:
         AWS_QUERYSTRING_AUTH = (
             os.environ.get("AWS_QUERYSTRING_AUTH") == "True"
         )
-    if os.environ.get("AWS_LOCATION"):
+    if os.environ.get("AWS_LOCATION", None) is not None:
         AWS_LOCATION = os.environ.get("AWS_LOCATION").strip("/") + "/"
-    STATICFILES_STORAGE = "storages.backends.s3boto3.S3StaticStorage"
+    # STATICFILES_STORAGE = "storages.backends.s3boto3.S3StaticStorage"
     SASS_PROCESSOR_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
