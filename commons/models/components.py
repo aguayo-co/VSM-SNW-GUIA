161ba751aff45432ca97f09842ecfc7db4690096
julian.tolosa2 <julian.tolosa@aguayo.co>
2022-08-18 14:54:59 -0500

    Banner AD component created.
    Banner AD created and added component block ID to create a scroll to button. Example in banner_ad_component.html.

--- commons/models/components.py
+++ commons/models/components.py
@@ -246,3 +246,17 @@ class ThematicContentComponent(StructBlock):
         label = _("Contenidos Tematicos")
         icon = "list-ul"
         template = "components/thematic_content.html"
+
+
+class BannerAdComponent(StructBlock):
+    """
+    A block that displays a banner ad.
+    """
+    banner_text = RichTextBlock(required=False, label=_("Texto de banner"), editor="basic")
+    link_text_scroll_to = CharBlock(required=False, label=_("Texto de enlace scroll to"))
+    id_link_scroll_to = CharBlock(required=False, label=_("ID de enlace scroll to"))
+
+    class Meta:
+        icon = "image"
+        label = _("Banner Ad")
+        template = "commons/components/banner_ad_component.html"
