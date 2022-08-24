d96c237143a6f1dcea493c927fc4b357e04f71ae
luis.tafur <luis.tafur@aguayo.co>
2022-08-24 15:18:53 -0500

    Fix (Swiper hero)
    - Se ajusta la instancia del swiper para el hero
    - Se agrega clase para eliminar margen y linea cuando solo hay un slider

--- vsm_snw/static/js/components/swiperSlider.js
+++ vsm_snw/static/js/components/swiperSlider.js
@@ -2,6 +2,61 @@ import Swiper, { Navigation, Pagination } from 'swiper';
 
 export default renderSliders => {
   sliderTestimonials();
+  sliderHero();
+}
+
+function sliderHero() {
+  let el = document.querySelector(".js-slider-hero");
+
+  if (el) {
+    var nextEl;
+    var prevEl;
+    var wrapper = el.closest(".js-slide-wrapper");
+
+    if (wrapper) {
+      nextEl = wrapper.querySelector(".swiper-button-next");
+      prevEl = wrapper.querySelector(".swiper-button-prev");
+    } else {
+      nextEl = this.element.querySelector(".swiper-button-next");
+      prevEl = this.element.querySelector(".swiper-button-prev");
+    }
+
+    new Swiper(el, {
+
+      // configure Swiper to use modules
+      modules: [Navigation, Pagination],
+
+      slidesPerView: 1,
+      spaceBetween: 2,
+      effect: "fade",
+
+      navigation: {
+        nextEl: nextEl,
+        prevEl: prevEl,
+        disabledClass: 'swiper-button-disabled'
+      },
+
+      pagination: {
+        el: ".l-slide__footer",
+        type: "custom",
+        renderCustom: function (swiper, current, total) {
+          const hero_pagination_texts = JSON.parse(document.getElementById("hero_pagination_texts").textContent)
+          prevEl.children[1].textContent = hero_pagination_texts[current].prev
+          nextEl.children[0].textContent = hero_pagination_texts[current].next
+        }
+      },
+
+      // Custom functions
+      on: {
+        init: function () {
+          if (this.slides.length <= 1) {
+            this.el.classList.add("is-not-slider-hero");
+          }
+        }
+      }
+
+    });
+  }
 }
 
 function sliderTestimonials() {
