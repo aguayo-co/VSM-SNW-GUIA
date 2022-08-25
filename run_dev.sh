90ac1433897472fde8899ab5469390bf029e00e7
Ramiro Alvarez <ramiro.alvarez@aguayo.co>
2022-08-24 20:18:49 -0500

    Fix(Deploy) - Ejecucion secuencial

--- run_dev.sh
+++ run_dev.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 
-python manage.py migrate && \
-python manage.py compilescss --use-storage && \
-python manage.py collectstatic --no-input && \
+echo "Running Django migrations (background job)" && \
+python manage.py migrate --noinput
+echo "Running Django Sass Compilation (background job)" && \
+python manage.py compilescss --use-storage
+echo "Running Django Static Collect (background job)" && \
+python manage.py collectstatic --no-input
+echo "Running Django Server" && \
 python -Wd manage.py runserver --nostatic 0.0.0.0:8000
