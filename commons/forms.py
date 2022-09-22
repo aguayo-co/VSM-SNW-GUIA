7189233e730b546c63b83a98a8521a7936199199
andres.tafur <luis.tafur@aguayo.co>
2022-09-22 09:44:04 -0500

    - Cambia los selects por multipleselect en formulario de filtros

--- commons/forms.py
+++ commons/forms.py
@@ -1,10 +1,10 @@
-from django.forms import Form, ModelChoiceField
+from django.forms import Form, ModelChoiceField, ModelMultipleChoiceField
 
 from django.utils.translation import gettext_lazy as _
 from commons.models import Degree, Subject, Serie
 
 
 class CatalogFilterForm(Form):
-    grade = ModelChoiceField(Degree.objects, label=_("Grados"), required=False)
-    subject = ModelChoiceField(Subject.objects, label=_("Materias"), required=False)
-    serie = ModelChoiceField(Serie.objects, label=_("Series"), required=False)
+    grade = ModelMultipleChoiceField(Degree.objects, label=_("Grados"), required=False)
+    subject = ModelMultipleChoiceField(Subject.objects, label=_("Materias"), required=False)
+    serie = ModelMultipleChoiceField(Serie.objects, label=_("Series"), required=False)
