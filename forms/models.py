ef2814004a2640a0a2036d1e05ba13694c0ab4fb
Andres Tafur <luis.tafur@aguayo.co>
2022-10-07 11:37:21 -0500

    Feature(Página formulario) - Identificar campos del formulario y texto enriquecido
    - Sobreescribe el widget de checkbox para t&c y captcha
    - Agrega texto enriquecido de intro para la página

--- forms/models.py
+++ forms/models.py
@@ -1,6 +1,8 @@
 from django.forms import BooleanField
+from django.forms.widgets import CheckboxInput
 from django.db import models
 from django.utils.translation import gettext_lazy as _
+from commons.models.fields import DetailProductIntroStreamField
 from modelcluster.fields import ParentalKey
 from wagtail.admin.panels import InlinePanel, FieldPanel
 from wagtail.contrib.forms.forms import FormBuilder
@@ -11,8 +13,17 @@ from wagtail.contrib.forms.models import (
     FORM_FIELD_CHOICES,
 )
 
-from commons.models import BasePage, FullStreamField
 from commons.models.custom_settings import SantillanaSettings
+
+
+class TermsAndConditionsCheckboxInput(CheckboxInput):
+    """Custom input widget for terms and conditions field."""
+    pass
+
+
+class CaptchaCheckboxInput(CheckboxInput):
+    """Custom input widget for captcha field."""
+    pass
 
 
 class FormField(AbstractFormField):
@@ -34,10 +45,15 @@ class CustomFormBuilder(FormBuilder):
                 Site.objects.first()
             ).terms_and_conditions,
             required=field.required,
+            widget=TermsAndConditionsCheckboxInput,
         )
 
     def create_captcha_field(self, field, options):
-        return BooleanField(label=_("¿Eres humano?"), required=field.required)
+        return BooleanField(
+            label=_("¿Eres humano?"),
+            required=field.required,
+            widget=CaptchaCheckboxInput,
+        )
 
 
 class FormPage(AbstractForm):
@@ -45,9 +61,11 @@ class FormPage(AbstractForm):
 
     CONTENT_FIELD = "_content_form"
 
-    _content_form = FullStreamField(verbose_name=("Contenido"), null=True, blank=True)
+    _content_form = DetailProductIntroStreamField(
+        verbose_name=("Intro"), null=True, blank=True)
 
     content_panels = AbstractForm.content_panels + [
+        FieldPanel(CONTENT_FIELD),
         InlinePanel("form_fields", label=_("Campos del Formulario")),
     ]
 
@@ -57,6 +75,23 @@ class FormPage(AbstractForm):
             Site.objects.first()
         ).key_recaptcha
         return context
+
+    @classmethod
+    def replace_content_field(cls, field_name):
+        """Replace content field in admin and `content` property interface."""
+        content_panels = []
+        for panel in cls.content_panels:
+            if panel.field_name != cls.CONTENT_FIELD:
+                content_panels.append(panel)
+                continue
+            content_panels.append(FieldPanel(field_name))
+
+        return content_panels
+
+    @property
+    def content(self):
+        """Devuelve el campo de contenido definido en CONTENT_FIELD."""
+        return getattr(self, self.CONTENT_FIELD)
 
     class Meta:
         verbose_name = _("Formulario")
