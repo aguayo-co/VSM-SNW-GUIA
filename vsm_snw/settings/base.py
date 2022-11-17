2a6c03642e2ab479a53b102f902e66bd6f98f717
Ramiro Alvarez <ramiro.alvarez@aguayo.co>
2022-11-16 22:08:16 -0500

    Feature(Static Files): Fix manifest file

--- vsm_snw/settings/base.py
+++ vsm_snw/settings/base.py
@@ -167,8 +167,8 @@ STATICFILES_DIRS = [
 # JavaScript / CSS assets being served from cache (e.g. after a Wagtail upgrade).
 # See https://docs.djangoproject.com/en/4.0/ref/contrib/staticfiles/#manifeststaticfilesstorage
 STATICFILES_STORAGE = "django.contrib.staticfiles.storage.ManifestStaticFilesStorage"
-if DEBUG == False:
-    STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+# if DEBUG == False:
+#     STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 STATIC_URL = "/static/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
