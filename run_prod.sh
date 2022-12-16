08a5887dc661e6d1caf62a910d82b526e2ad0e9c
Ramiro Alvarez <ramiro.alvarez@aguayo.co>
2022-12-16 11:53:43 -0500

    Feature(Elastic): - add index update

--- run_prod.sh
+++ run_prod.sh
@@ -4,4 +4,5 @@ python manage.py migrate --noinput
 python manage.py compilescss --use-storage
 python manage.py collectstatic --no-input
 python manage.py compilemessages
+python manage.py update_index
 gunicorn vsm_snw.wsgi -b 0.0.0.0:8000
